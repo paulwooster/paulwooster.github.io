5de062aadf69bfbe0e315aec6fac2a98a157eb04
Paul Wooster <paul.wooster@gmail.com>
2023-09-06 10:25:42 -0500

    Move tile constants into separate file, other cleanup

--- games/assets/terrain/tree.js
+++ games/assets/terrain/tree.js
@@ -1,21 +1,32 @@
-let tiles =  
-{
-  "terrain": [
-    "",
-    "ramp",
-    "rampLarge",
-    "rampLarge_detailed",
-    "roadCorner",
-    "roadCross",
-    "roadEnd",
-    "roadSplit",
-    "roadStraight",
-    "sideCliff",
-    "sideCornerInner",
-    "sideCorner",
-    "sideEnd",
-    "side"
-  ]
-};
+/* Define various parameters related to tiles in this folder */
 
-export default tiles;
+export const terrain = [
+    "",
+    "_ramp",
+    "_rampLarge",
+    "_rampLarge_detailed",
+    "_roadCorner",
+    "_roadCross",
+    "_roadEnd",
+    "_roadSplit",
+    "_roadStraight",
+    "_sideCliff",
+    "_sideCornerInner",
+    "_sideCorner",
+    "_sideEnd",
+    "_side"
+  ];
+
+export const terrain_suffix = ["_NW", "_NE", "_SE", "_SW"];
+
+export const terrain_anchor = [0.5, 0.615];
+
+export const terrain_halfWidth = 64;
+export const terrain_halfHeight = 32;
+
+export const terrain_hitArea = [
+    terrain_halfWidth, 0,
+    0, terrain_halfHeight,
+    -terrain_halfWidth, 0,
+    0, -terrain_halfHeight
+  ];
--- games/first_pixi/index.js
+++ games/first_pixi/index.js
@@ -1,4 +1,5 @@
-import terrain_tree from '../assets/terrain/tree.js';
+// //@ts-check
+import {terrain, terrain_suffix, terrain_anchor, terrain_hitArea} from '../assets/terrain/tree.js';
 
 let app =  new PIXI.Application({ width: 512, height: 512});
 
@@ -7,33 +8,28 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 
 let sprite = PIXI.Sprite.from('../assets/terrain/terrain_roadCross_NE.png');
 
-sprite.anchor.set(0.5,0.615);
-sprite.hitArea = new PIXI.Polygon(
-    64,0,
-    0,32,
-    -64,0,
-    0,-32
-);
+sprite.anchor.set(...terrain_anchor);
+sprite.hitArea = new PIXI.Polygon(...terrain_hitArea);
 
 sprite.eventMode = "dynamic";
-sprite.on('pointerdown', (event) => { alert('clicked!'); });
+sprite.on('pointerdown', (event) => { moving = !moving; });
 app.stage.addChild(sprite);
 windowResize();
 
 // Add a variable to count up the seconds our demo has been running
 let elapsed = 0.0;
-const MAX_ELAPSED = 500;
+let moving = true;
 
 app.ticker.add((delta) => {
     // Add the time to our total elapsed time
-    if (elapsed < MAX_ELAPSED) {
+    if (moving) {
         elapsed += delta;
     }
 
     // Update the sprite's X position based on the cosine of our elapsed time.  We divide
     // by 50 to slow the animation down a bit...
-    sprite.x = app.screen.width/2 + Math.cos(elapsed/50.0) * app.screen.width/2 * 0.9;
-    sprite.y = app.screen.height/2 + Math.sin(elapsed/25.0) * app.screen.height/2 * 0.9;
+    sprite.x = app.screen.width/2 + Math.cos(elapsed/150.0) * app.screen.width/2 * 0.9;
+    sprite.y = app.screen.height/2 + Math.sin(elapsed/75.0) * app.screen.height/2 * 0.9;
 });
 
 // Fullscreen in pixi is resizing the renderer to be window.innerWidth by window.innerHeight
@@ -58,4 +54,5 @@ function leaveFullscreen () {
     }
 }
 
-console.log(terrain_tree);
+console.log(terrain);
+console.log(terrain_suffix);
