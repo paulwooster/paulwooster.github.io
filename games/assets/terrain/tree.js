e0914c386e439c03fd9c1f6698c8957c5d424bc0
Paul Wooster <paul.wooster@gmail.com>
2023-09-23 18:51:27 -0500

    Streamline code and add buttons

--- games/assets/terrain/tree.js
+++ games/assets/terrain/tree.js
@@ -1,21 +1,21 @@
 /* Define various parameters related to tiles in this folder */
 
 export const terrain = [
-    "",
-    "_ramp",
-    "_rampLarge",
-    "_rampLarge_detailed",
-    "_roadCorner",
-    "_roadCross",
-    "_roadEnd",
-    "_roadSplit",
-    "_roadStraight",
-    "_sideCliff",
-    "_sideCornerInner",
-    "_sideCorner",
-    "_sideEnd",
-    "_side"
-  ];
+  "",
+  "_ramp",
+  "_rampLarge",
+  "_rampLarge_detailed",
+  "_roadCorner",
+  "_roadCross",
+  "_roadEnd",
+  "_roadSplit",
+  "_roadStraight",
+  "_sideCliff",
+  "_sideCornerInner",
+  "_sideCorner",
+  "_sideEnd",
+  "_side"
+];
 
 export const terrain_suffix = ["_NW", "_NE", "_SE", "_SW"];
 
@@ -25,8 +25,8 @@ export const terrain_halfWidth = 64;
 export const terrain_halfHeight = 32;
 
 export const terrain_hitArea = [
-    terrain_halfWidth, 0,
-    0, terrain_halfHeight,
-    -terrain_halfWidth, 0,
-    0, -terrain_halfHeight
-  ];
+  terrain_halfWidth, 0,
+  0, terrain_halfHeight,
+  -terrain_halfWidth, 0,
+  0, -terrain_halfHeight
+];
