c414d31c165b27ce131e978479a429e9e437fb3f
Paul Wooster <paul.wooster@gmail.com>
2023-09-24 15:26:52 -0500

    Programmatically fetching images

--- games/assets/terrain/tree.js
+++ games/assets/terrain/tree.js
@@ -1,6 +1,6 @@
 /* Define various parameters related to tiles in this folder */
 
-export const terrain = [
+const terrain = [
   "",
   "_ramp",
   "_rampLarge",
@@ -17,14 +17,23 @@ export const terrain = [
   "_side"
 ];
 
-export const terrain_suffix = ["_NW", "_NE", "_SE", "_SW"];
+const flatTerrain = [
+  "",
+//  "_roadCorner",
+  "_roadCross",
+  "_roadEnd",
+//  "_roadSplit",
+  "_roadStraight",
+];
 
-export const terrain_anchor = [0.5, 0.615];
+const terrain_suffix = ["_NW", "_NE", "_SE", "_SW"];
 
-export const terrain_halfWidth = 64;
-export const terrain_halfHeight = 32;
+const terrain_anchor = [0.5, 0.615];
 
-export const terrain_hitArea = [
+const terrain_halfWidth = 64;
+const terrain_halfHeight = 32;
+
+const terrain_hitArea = [
   terrain_halfWidth, 0,
   0, terrain_halfHeight,
   -terrain_halfWidth, 0,
